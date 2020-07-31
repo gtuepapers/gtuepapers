db8c1e07a846775f0da1d55cd0a0e9bb59b96b17
Khushit Shah <khushitshah1@gmail.com>
2020-07-31 12:17:15 +0530

    Updated python script

--- backend/python-scrapper/fetch_papers_code_url.py
+++ backend/python-scrapper/fetch_papers_code_url.py
@@ -1,19 +1,20 @@
 # [code, url]
+import sys
+import json
+import time
+from pathlib import Path
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
 allPapers = list()
 # [url]
 crawled = set()
 # [code] [branch][code]
 codes = dict()
 
-branches = set("BA,BC,BD,BE,BESP,BH,BI,BL,BM,BP,BPSP,BT,BV,DA,DI,DISP,DP,DV,FD,HM,IC,MA,MB,MC,MCSP,ME,ML,MN,MP,MR,MT,PD,PH,PM,PP,PR,TE,VM".split(","))
+branches = "BE,BA,BC,BD,BESP,BH,BI,BL,BM,BP,BPSP,BT,BV,DA,DI,DISP,DP,DV,FD,HM,IC,MA,MB,MC,MCSP,ME,ML,MN,MP,MR,MT,PD,PH,PM,PP,PR,TE,VM".split(
+    ",")
 print(branches)
-
-from urllib.parse import urljoin
-from bs4 import BeautifulSoup as bs
-import requests
-from pathlib import Path
-import time
-import json
 
 
 def savePaperJson():
@@ -26,51 +27,52 @@ def savePaperJson():
     file.write(papers_json)
     file.close()
 
+
 def page(url, check=False):
     global allPapers
     # check if site is indeed an PDF Or Zip
     status_code = 200
     content_type = "pdf"
-        # don't add if year < 2014, code length < 7
+    # don't add if year < 2014, code length < 7
     # Split Paper link with '/' to get Department and Year.. And also remove prefix *http://old.gtu.ac.in/GTU_Papers/*
 
-
-    if check :
+    if check:
         res = requests.head(url)
         status_code = res.status_code
         content_type = res.headers["Content-Type"]
 
-    if ((not content_type.__contains__("html")  and status_code < 400) and (url.endswith(".zip") or url.endswith(".pdf"))):
+    if ((not content_type.__contains__("html") and status_code < 400) and (url.endswith(".zip") or url.endswith(".pdf"))):
         t2 = url.split("/")
         t2 = t2[t2.__len__() - 1]
-        urlSplit = ""
-        if(url.startswith("http://old.gtu.ac.in/GTU_Papers/")):
-            urlSplit = url.split(
-                "http://old.gtu.ac.in/GTU_Papers/")[1].split("/")
-        else:
-            urlSplit = url.split(
-                "http://files.gtu.ac.in/GTU_Papers/")[1].split("/")
-        # Check Year
+        if not check:
+            urlSplit = ""
+            if(url.startswith("http://old.gtu.ac.in/GTU_Papers/")):
+                urlSplit = url.split(
+                    "http://old.gtu.ac.in/GTU_Papers/")[1].split("/")
+            else:
+                urlSplit = url.split(
+                    "http://files.gtu.ac.in/GTU_Papers/")[1].split("/")
+            # Check Year
 
-        urlSplit[1] = urlSplit[1].replace("_", " ")
-        if urlSplit[1][-4:].isnumeric() and int(urlSplit[1][-4:]) < 2014:
-            return False
+            urlSplit[1] = urlSplit[1].replace("_", " ")
+            if urlSplit[1][-4:].isnumeric() and int(urlSplit[1][-4:]) < 2014:
+                return False
 
-        # Remove .pdf or .zip from subject code
-        urlSplit[urlSplit.__len__(
-        ) - 1] = urlSplit[urlSplit.__len__() - 1].replace(".pdf", "")
-        urlSplit[urlSplit.__len__(
-        ) - 1] = urlSplit[urlSplit.__len__() - 1].replace(".zip", "")
+            # Remove .pdf or .zip from subject code
+            urlSplit[urlSplit.__len__(
+            ) - 1] = urlSplit[urlSplit.__len__() - 1].replace(".pdf", "")
+            urlSplit[urlSplit.__len__(
+            ) - 1] = urlSplit[urlSplit.__len__() - 1].replace(".zip", "")
 
-        if len(urlSplit[2]) < 7:
-            return False
+            if len(urlSplit[2]) < 7:
+                return False
 
         code = t2.split(".")[0]
         for branch in branches:
             # if url.__contains__(branch):
-                if not codes.__contains__(branch):
-                    codes[branch] = list()
-                codes[branch].append(code)
+            if not codes.__contains__(branch):
+                codes[branch] = set()
+            codes[branch].add(code)
 
         curPaper = [code, url]
 
@@ -87,10 +89,10 @@ def rec(baseURL):
     basePage = requests.get(baseURL).text
     aTags = bs(basePage, "html.parser").find_all("a")
     for a in aTags:
-        hreflink = urljoin(baseURL,a.get("href"))
+        hreflink = urljoin(baseURL, a.get("href"))
         valid = ((
             hreflink.startswith("http://www.gtu.ac.in")) or (hreflink.startswith(
-        "http://old.gtu.ac.in")) or (hreflink.startswith("http://files.gtu.ac.in")))
+                "http://old.gtu.ac.in")) or (hreflink.startswith("http://files.gtu.ac.in")))
         if((hreflink == "http://www.gtu.ac.in") or (hreflink == "http://gtu.ac.in/Download1.aspx") or (hreflink == "http://www.gtu.ac.in/")):
             continue
         else:
@@ -101,27 +103,26 @@ def rec(baseURL):
             else:
                 print("Link not in crawling domain!", hreflink)
 
-# recursively got for all the papers!            
+# recursively got for all the papers!
 base = "http://old.gtu.ac.in/Qpaper.html"
 rec(base)
 savePaperJson()
 
+print(len(codes["BE"]))
 
-base =  "https://www.gtu.ac.in/uploads"
-sems = ["W2019","S2018","W2018","S2019", "W2017"]
-prefix = []
-for branch in branches:
-    prefix[branch] = {}
+base = "https://www.gtu.ac.in/uploads"
+sems = ["W2019", "S2018", "W2018", "S2019", "W2017"]
 
 for sem in sems:
     for branch in branches:
-      try:
-        for code in codes[branch]:
-            if not code[:3] in prefix[branch] 
-                ans = page(base+"/"+sem+"/"+branch+"/"+code+".pdf", check=True)
-                if not ans:
-                    prefix[branch].add(code[:3])
-      except:
-        continue
+        # try:
+            for code in codes[branch]:
+                # try:
+                        ans = page(base+"/"+sem+"/"+branch +
+                                   "/"+code+".pdf", check=True)
+                # except:
+                    # print(sys.exc_info()[0])
+        # except:
+            # print(sys.exc_info()[0])
 
 savePaperJson()
